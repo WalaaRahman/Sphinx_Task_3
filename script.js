ea0b4cc1b89c71649ba98c66e485415eaede7fbf
WalaaRahman <w.gohary46@gmail.com>
2022-01-11 21:14:57 +0200

    task 3

---
+++ script.js
@@ -0,0 +1,7 @@
+setTimeout(function gif() {
+    document.getElementById("loading_img").style.display = "none";
+}, 600)
+
+setTimeout(function content() {
+    document.getElementById("content").style.display = "block";
+}, 550)
